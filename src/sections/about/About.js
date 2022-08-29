80873156c0abccbd0fc3b4ecce7ed6da6d6d068e
Abdi-kodehode <abdi.kodehode@gmail.com>
2022-08-29 14:45:06 +0200

    update bio v2

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -9,11 +9,13 @@ const About = () => {
         <div>
           <article>
             <p>
-              Hei, jeg er Abdi. Jeg er en webutvikler, interessert i fullstack web utvikling. Å se koden du har skrevet bli til   noe var det som inspirerte meg til å begynne på kodehode.
+              Hei, jeg er Abdi. Jeg er en webutvikler, interessert i alt som har med fullstack delen og spesielt backend. Å se koden du har skrevet bli til noe var det som inspirerte meg til å begynne på kodehode.
             </p><br />
             <p>
-              Alt startet 7 mars 2022 med enkel HTML og har raskt gått videre til react og nylig nodeJS.
-              Jeg er en velorganisert person, en problemløser og en god teamplayer med god oppmerksomhet for detaljer. Fan av fotball, spiller en del mmo spill og digger syfy/adventure filmer. 
+              Alt startet 7 mars 2022, med ingen forkunnskap for frontend eller noe webutvikling. Ved å følge opplegget som var satt opp av kodehode så var det rett fram og oversiktlig. HTML, CSS, JavaScript samt React har gått ganske stødig. Startet nylig med node JS og har fått sansen for backend. 
+            </p><br />
+            <p>
+              Ellers er jeg en velorganisert person, en problemløser og en god teamplayer med god oppmerksomhet for detaljer. På fritiden spiller jeg en del mmorpg spill og digger syfy/adventure filmer. 
               En familieorientert person og far til 2 rakkerunger. 
               La oss lage noe sammen
             </p>
