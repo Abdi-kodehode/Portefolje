cfc688ac9de437f8f474694a4165fbf32f2e058d
Abdi-kodehode <abdi.kodehode@gmail.com>
2022-08-29 11:13:52 +0200

    mail update

--- src/sections/socialmedia/Icons.js
+++ src/sections/socialmedia/Icons.js
@@ -5,10 +5,10 @@ import './Icons.css'
 const Icons = () => {
     return (
       <div className="iconBar">
-          <a href="https://github.com" rel="noreferrer" target="_blank"><SiGithub size={30} color='#fff' /></a>
+          <a href="https://github.com/Abdi-kodehode" rel="noreferrer" target="_blank"><SiGithub size={30} color='#fff' /></a>
+          <a href="https://mail.google.com" mailto="abdifatah.hersi@gmail.com" ><SiGmail size={30} color='#fff' /></a>
           <a href="https://no.linkedin.com/" rel="noreferrer" target="_blank"><SiLinkedin size={30} color='#fff' /></a>
           <a href="https://twitter.com/i/flow/login" rel="noreferrer" target="_blank"><SiTwitter size={30} color='#fff' /></a>
-          <a href="https://mail.google.com" rel="noreferrer" target="_blank"><SiGmail size={30} color='#fff' /></a>
       </div>
     )
   }
