3dfaae2608a0b23caca1817de20cc0dbc485ddff
Abdi-kodehode <abdi.kodehode@gmail.com>
2022-09-05 12:33:35 +0200

    small typography change

--- src/sections/about/About.js
+++ src/sections/about/About.js
@@ -16,7 +16,10 @@ const About = () => {
               I Mars 2022, fikk jeg muligheten til å starte på kurset; <i>Kodehode</i>. I løpet av kurset har jeg tilegnet meg masse ny kunnskap, der i blant HTML/CSS, JavaScript og React. Kurset har vært med på å utvikle meg fra en med lite kode kunnskap til en med masse kode kunnskap. Nylig startet jeg med Node.js, og det ga meg virkelig sansen for backend. 
             </p><br />
             <p>
-              Ellers er jeg en velorganisert person, en problemløser og en god teamplayer med øye for detaljer. På fritiden spiller jeg en del mmorpg spill og digger sci-fi/adventure filmer. Jeg er familieorientert og far til 2 rakkerunger. La oss lage noe sammen.
+              Ellers er jeg en velorganisert person, en problemløser og en god teamplayer med øye for detaljer. På fritiden spiller jeg en del mmorpg spill og digger sci-fi/adventure filmer. Jeg er familieorientert og far til 2 rakkerunger. 
+            </p><br />
+            <p>
+              <strong><i>La oss lage noe sammen.</i></strong>
             </p>
           </article>
         </div>
